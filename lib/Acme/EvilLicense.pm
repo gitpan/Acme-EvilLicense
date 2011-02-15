1e5ea80d52a6a72ff68259938daa06e04c7e0296
Кирилл Мязин <km@krot.org>
2011-02-15 22:50:46 +0000

    Import of KIRILLM/Acme-EvilLicense-0.01 from CPAN.
    gitpan-cpan-distribution: Acme-EvilLicense
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KIRILLM/Acme-EvilLicense-0.01.tgz
    gitpan-cpan-author:       KIRILLM
    gitpan-cpan-maturity:     released

---
+++ lib/Acme/EvilLicense.pm
@@ -0,0 +1,43 @@
+package Acme::EvilLicense;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Acme::EvilLicense - The great new Acme::EvilLicense!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Acme::EvilLicense;
+
+    my $foo = Acme::EvilLicense->new();
+    ...
+
+Yadda yadda yadda.
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2011 Evil Hacker.
+
+This program is released under the following license: evil
+
+Please see the README file provided with the distribution for licensing
+terms and usage conditions.
+
+=cut
+
+1; # End of Acme::EvilLicense
